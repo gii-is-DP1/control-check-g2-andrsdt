da25930a132a21ece6715b61747240991e0e0d41
andrsdt <andresdt@pm.me>
2022-01-14 09:34:42 +0100

    fix: test1

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -4,12 +4,15 @@ import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.Pet;
 
 import lombok.Getter;
@@ -19,10 +22,10 @@ import lombok.Setter;
 @Setter
 @Entity
 @Table(name = "feedings")
-public class Feeding extends BaseEntity { // TODO extend baseEntity if not working
-    // @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
-    // Integer id;
+public class Feeding {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
 
     @NotNull // "The attribute must be required"
     @DateTimeFormat(pattern = "yyyy/MM/dd")
@@ -30,11 +33,12 @@ public class Feeding extends BaseEntity { // TODO extend baseEntity if not worki
     LocalDate startDate;
 
     @NotNull
+    @Min(1)
+    @Column(name = "weeks_duration")
     double weeksDuration;
 
-    // TODO This is N-1 instead of 1-N?
     @NotNull
-    @ManyToOne // TODO missing "mappedBy"?
+    @ManyToOne
     Pet pet;
 
     @ManyToOne
