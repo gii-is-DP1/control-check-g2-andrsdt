5589d4324afcc0902159b9da4e0d3dc53cd4dbd1
andrsdt <andresdt@pm.me>
2022-01-14 08:49:21 +0100

    add: test1

--- src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/Feeding.java
@@ -2,6 +2,14 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.ConstraintViolationException;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.Pet;
 
 import lombok.Getter;
@@ -9,9 +17,22 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Feeding {
-    Integer id;
+@Entity
+public class Feeding extends BaseEntity { // TODO extend baseEntity if not working
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // Integer id;
+
+    @NotNull // "The attribute must be required"
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    @Column(name = "start_date")
     LocalDate startDate;
-    double weeksDuration;
-    Pet pet;   
+
+    @NotNull
+    Double weeksDuration;
+
+    // TODO This is N-1 instead of 1-N?
+    @NotNull
+    @ManyToOne // TODO missing "mappedBy"?
+    Pet pet;
 }
